6a8958846b95a53cc009a819c4eb44da21a2bfa1
Jeff Garzik <jgarzik@localhost.localdomain>
2020-09-19 05:44:22 +0000

    qualify uniswap

--- contracts/QualifyLP.sol
+++ contracts/QualifyLP.sol
@@ -9,11 +9,31 @@ import './interfaces/IUniswapV2Pair.sol';
 
 contract QualifyLP is IQualifyLP {
     mapping(address => uint256) private lps;
+    uint256 constant LP_UNISWAP = 1;
+    uint256 constant LP_BALANCER = 2;
 
     constructor() public {
     }
 
     function qualified(address lpToken) external override view returns(uint256) {
         return lps[lpToken];
+    }
+
+    function qualifyUniswapLP(address lpToken, address token0, address token1) external override returns (bool) {
+        IUniswapV2Pair pair = IUniswapV2Pair(lpToken);
+
+	require(pair.decimals() == 18);
+	require(pair.totalSupply() > 0);
+	require(token0 != token1);
+	require(pair.token0() == token0 || pair.token1() == token0);
+	require(pair.token0() == token1 || pair.token1() == token1);
+
+	(uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) =
+	    pair.getReserves();
+	require(reserve0 > 0);
+	require(reserve1 > 0);
+        require(blockTimestampLast > 0);
+
+	lps[lpToken] = LP_UNISWAP;
     }
 }
