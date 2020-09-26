ff52e0be0ea7ef5d31d6e6bf6551592074ec41a1
Jeff Garzik <jgarzik@localhost.localdomain>
2020-09-26 22:58:50 +0000

    QualifyLP: add code comments

--- contracts/QualifyLP.sol
+++ contracts/QualifyLP.sol
@@ -16,11 +16,21 @@ contract QualifyLP is IQualifyLP {
     }
 
     function qualified(address lpToken) external override view returns(uint256) {
+	// return qualification data for the specified contract address.
+	// returns zero, if unknown / not-qualified.
         return lps[lpToken];
     }
 
     function qualifyUniswapLP(address lpToken, address token0, address token1) external override returns (bool) {
         IUniswapV2Pair pair = IUniswapV2Pair(lpToken);
+
+	// Perform a series of simple functional tests, calling
+	// as many different ABI functions as is feasible without
+	// exceeding gas limits or performing complex operations that
+	// require additional assets.
+
+	// Each ABI call, and each test, increases the likelihood that
+	// this contract is a Uniswap pair contract.
 
 	require(pair.decimals() == 18);
 	require(pair.totalSupply() > 0);
@@ -34,6 +44,7 @@ contract QualifyLP is IQualifyLP {
 	require(reserve1 > 0);
         require(blockTimestampLast > 0);
 
+	// Tests complete.  Mark token as a Uniswap LP token contract.
 	lps[lpToken] = LP_UNISWAP;
 
 	return true;
@@ -42,12 +53,21 @@ contract QualifyLP is IQualifyLP {
     function qualifyBalancerLP(address lpToken) external override returns (bool) {
 	BPool bpool = BPool(lpToken);
 
+	// Perform a series of simple functional tests, calling
+	// as many different ABI functions as is feasible without
+	// exceeding gas limits or performing complex operations that
+	// require additional assets.
+
+	// Each ABI call, and each test, increases the likelihood that
+	// this contract is a (finalized+swappable) Balancer pool contract.
+
 	require(bpool.totalSupply() > 0);
 	require(bpool.isPublicSwap() == true);
 	require(bpool.isFinalized() == true);
 	require(bpool.getNumTokens() >= 2);
 	require(bpool.getSwapFee() > 0);
 
+	// Tests complete.  Mark token as a Balancer LP token contract.
 	lps[lpToken] = LP_BALANCER;
 
 	return true;
